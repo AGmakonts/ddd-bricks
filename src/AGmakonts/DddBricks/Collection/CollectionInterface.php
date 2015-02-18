f80c5469580177c983dcb3e0ed2be58679619601
Adam <adam@grabek.io>
2015-02-18 19:12:55 +0100

    Collection interface

---
+++ src/AGmakonts/DddBricks/Collection/CollectionInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: adamgrabek
+ * Date: 18/02/15
+ * Time: 19:12
+ */
+namespace AGmakonts\DddBricks\Collection;
+
+
+/**
+ * Interface CollectionInterface
+ *
+ * @author    Adam Grabek <adam@procreative.eu>
+ * @copyright 1985 - 2015 Kelleher, Helmrich and Associates, Inc.
+ */
+interface CollectionInterface
+{
+
+}
