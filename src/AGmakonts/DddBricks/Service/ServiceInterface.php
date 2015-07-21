c88b747184ab5c9482dc203d19043c73adff74ec
Radek Adamiec <radek@procreative.eu>
2015-07-21 15:31:25 +0200

    Added new interface for services

---
+++ src/AGmakonts/DddBricks/Service/ServiceInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Radek Adamiec
+ * Date: 21.07.15
+ */
+
+namespace AGmakonts\DddBricks\Service;
+
+/**
+ * Class ServiceInterface
+ *
+ * @package AGmakonts\DddBricks\Service
+ */
+class ServiceInterface
+{
+}
